39f12cde2c71b1e94b95046418af9a9119342485
cristinAlvestegui <quitina.23@gmail.com>
2023-02-03 20:15:14 -0300

    pastas

---
+++ PHP/Modelo/Employee/Funcionario.php
@@ -0,0 +1,34 @@
+<?php
+
+    require_once('Address.php');
+
+    class Funcionario extends Humano{
+        protected int $codigo;
+        protected string $sexo;
+        protected string $funcao;
+        protected float $salar;
+
+        public function __contruct(string $nome, string $tele, string $nasci, string $cpf, Address $address, int $codigo, string $sexo, string $funcao, float $salar){
+            parent::__construct($nome,  $tele,  $nasci,  $cpf, $address);
+            $this->codigo = $codigo;
+            $this->sexo   = $sexo;
+            $this->funcao = $funcao;
+            $this->salar  = $salar;
+        }
+
+        public function __get(string $qualQuer){
+            return $this->$qualQuer;
+        }
+
+        public function __set(string $qualQuer, string $valor){
+            $this->qualQuer = $valor;
+        }
+
+        public function __ToString(){
+            return "<br>Código:  ".$this->codigo."<br>Sexo:  ".$this->sexo."<br>Função:  ".$this->funcao."<br>Salário: ".$this->salar."R$";
+        }
+
+        /*Somente gerentes podem cadastrar funcionarios
+        */
+    }//Fim da classe funcionario
+?>
